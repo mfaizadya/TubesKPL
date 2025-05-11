024d25e99fd2b726b90e932e8d6b91883acabb34
RakaValrizqy <pendragonqword@gmail.com>
2025-05-11 20:19:47 +0700

    Admin Login API

---
+++ LoginAPI/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoginAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private static List<Admin> daftarAdmin = new List<Admin>
+        {
+            new Admin("Admin1", "admin", "password"),
+            new Admin{Nama="Admin2", Username="admin2", Password="password"}
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Admin>> GetAllAdmin()
+        {
+            return daftarAdmin;
+        }
+
+        [HttpPost("login")]
+        public ActionResult<Admin> Login([FromBody] LoginReq req)
+        {
+            int i;
+            for (i = 0; i < daftarAdmin.Count; i++)
+            {
+                if (daftarAdmin[i].Username == req.Username && daftarAdmin[i].Password == req.Password)
+                {
+                    return daftarAdmin[i];
+                }
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ LoginAPI/LoginReq.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginAPI
+{
+    public class LoginReq
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
