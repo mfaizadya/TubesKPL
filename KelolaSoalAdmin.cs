b7232b433c065f1c8a149291030e808dce517cfc
mfaizadya <m.faiz.adya@gmail.com>
2025-05-13 22:52:33 +0700

    Admin Kelola Soal (Table-driven)

---
+++ KelolaSoalAdmin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum AksiSoal { Tambah, Ubah, Hapus }
+
+public class KelolSoalAdmin
+{
+    private readonly Dictionary<(AksiSoal, JenisSoal), Action<Soal>> aksiTable;
+
+    public KelolSoalAdmin()
+    {
+        aksiTable = new Dictionary<(AksiSoal, JenisSoal), Action<Soal>>
+        {
+            { (AksiSoal.Tambah, JenisSoal.Esai), TambahSoalEsai },
+            { (AksiSoal.Tambah, JenisSoal.PilihanGanda), TambahSoalPG },
+            { (AksiSoal.Ubah, JenisSoal.Esai), UbahSoalEsai },
+            { (AksiSoal.Ubah, JenisSoal.PilihanGanda), UbahSoalPG },
+            { (AksiSoal.Hapus, JenisSoal.Esai), HapusSoalEsai },
+            { (AksiSoal.Hapus, JenisSoal.PilihanGanda), HapusSoalPG }
+        };
+    }
+
+    public void KelolaSoal(AksiSoal aksi, JenisSoal jenis, Soal data)
+    {
+        if (aksiTable.TryGetValue((aksi, jenis), out var aksiFunc))
+        {
+            aksiFunc(data);
+        }
+        else
+        {
+            Console.WriteLine("Aksi tidak dikenali.");
+        }
+    }
+
+    private void TambahSoalEsai(Soal soal) {  }
+    private void TambahSoalPG(Soal soal) {  }
+    private void UbahSoalEsai(Soal soal) {  }
+    private void UbahSoalPG(Soal soal) {  }
+    private void HapusSoalEsai(Soal soal) {  }
+    private void HapusSoalPG(Soal soal) { }
+}
---
+++ Soal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum JenisSoal { Esai, PilihanGanda }
+
+public class Soal
+{
+    public int Id { get; set; }
+    public JenisSoal Jenis { get; set; }
+    public string Pertanyaan { get; set; }
+    public string Jawaban { get; set; }
+    public List<string>? Opsi { get; set; }
+}
+
+
