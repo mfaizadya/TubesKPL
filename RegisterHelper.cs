a30b802a31188adcc6e85efb829ce6ff91c806ba
Rakunberacun <mrakan711@gmail.com>
2025-05-14 15:40:26 +0700

    menyimpan data users ke json dan menambah endpoint GET pada API

--- RegisterHelper.cs
+++ RegisterHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
 using TubesKPL;
 
@@ -13,33 +14,27 @@ public class RegisterHelper
 
     public static async Task SendRegisterRequest(RegisterRequest request, string role)
     {
-        var handler = new HttpClientHandler();
-        handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
-
-        using (HttpClient client = new HttpClient(handler))
+        using (HttpClient client = new HttpClient())
         {
-            client.Timeout = TimeSpan.FromSeconds(10);
-            string endpoint = $"https://localhost:7035/api/auth/{role}/register";
-
             try
             {
+                string endpoint = $"https://localhost:7035/api/auth/{role}/register";
                 HttpResponseMessage response = await client.PostAsJsonAsync(endpoint, request);
+
                 if (response.IsSuccessStatusCode)
                 {
                     Console.WriteLine("Registrasi berhasil!");
                 }
                 else
                 {
-                    Console.WriteLine($"Registrasi gagal! Status: {(int)response.StatusCode} - {response.ReasonPhrase}");
-
                     string error = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Registrasi gagal! Status: {(int)response.StatusCode}");
                     Console.WriteLine($"Pesan dari API: {error}");
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Terjadi kesalahan koneksi: {ex.Message}");
-                Console.WriteLine($"Detail: {ex.InnerException?.Message}");
+                Console.WriteLine("Terjadi kesalahan koneksi: " + ex.Message);
             }
         }
     }
--- obj/Debug/net8.0/TubesKPL.AssemblyInfo.cs
+++ obj/Debug/net8.0/TubesKPL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TubesKPL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5280540e7264740c1f46a93064d082cdfe7fa9f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d58a56e03cfe45411d2983a97f134e32deab0a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TubesKPL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TubesKPL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
